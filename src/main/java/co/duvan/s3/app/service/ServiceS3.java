946e96eca5aea0f1b470736a3ddbf34a0e2124e8
DUVAN0829 <duvthan067@gmail.com>
2026-01-02 12:38:49 -0500

    Añade dos métodos más en 'service/ServiceS3' para acceso temporal según se requiera

--- src/main/java/co/duvan/s3/app/service/ServiceS3.java
+++ src/main/java/co/duvan/s3/app/service/ServiceS3.java
@@ -2,6 +2,7 @@ package co.duvan.s3.app.service;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.List;
 
 public interface ServiceS3 {
@@ -15,5 +16,11 @@ public interface ServiceS3 {
     Boolean uploadFile(String bucketName, String key, Path fileLocation);
 
     void downloadFile(String bucket, String key) throws IOException;
+
+    //* Permite subir archivos por tiempo limitado o de manera temporal
+    String generatePresignedUploadUrl(String bucketName, String key, Duration duration);
+
+    //* Permite descargar archivos por tiempo limitado o de manera temporal
+    String generatePresignedDownloadUrl(String bucketName, String key, Duration duration);
 
 }
